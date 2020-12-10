5b545c29ac6bffec561bd87587fda25cfb9dd719
Bunks <busk_1@yahoo.com>
2020-12-09 19:03:02 -0500

    added more comments!

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  // need my length of password variable to be global so it can be accessed later.
   var lengthOfPassword;
   passwordText.value = password;
 
   function generatePassword() {
+    // setting up the basic arrays needed
     lowerCaseArray = [
       "a",
       "b",
@@ -67,44 +69,55 @@ function writePassword() {
     ];
     numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+    // created the combination array
     arrayOfArray = [];
-
+    // do while loop to ensure that the password is the proper length
     do {
       lengthOfPassword = prompt(
         "How long would your password, please choose between 8 and 128"
       );
     } while (lengthOfPassword > 128 || lengthOfPassword < 8);
+    // following four prompts are to ask if the user wants various special characters
     if (confirm("Do you want uppercase letters in your password?")) {
       arrayOfArray.push(upperCaseArray);
-      console.log(arrayOfArray);
+      // console.log(arrayOfArray);
     }
     if (confirm("Do you want lowercase letters in your password?")) {
       arrayOfArray.push(lowerCaseArray);
-      console.log(arrayOfArray);
+      // console.log(arrayOfArray);
     }
     if (confirm("Do you want special character in your password?")) {
       arrayOfArray.push(specialArray);
-      console.log(arrayOfArray);
+      // console.log(arrayOfArray);
     }
     if (confirm("Do you want numeric characters in your password?")) {
       arrayOfArray.push(numericArray);
-      console.log(arrayOfArray);
+      // console.log(arrayOfArray);
     }
+    // if no criteria is chosen it alerts the user.
     if (arrayOfArray.length < 1 || arrayOfArray == undefined) {
       alert("You didn't pick any criteria.");
     }
+    
+    // making the random password via for loops.
     for (let i = 0; i < lengthOfPassword; i++) {
       // Math.floor(Math.random() * arrayOfArray.length)
       var which = Math.floor(Math.random() * arrayOfArray.length);
+      console.log(arrayOfArray.length)
+      // console.log(which)
       var chosen = Math.floor(Math.random() * arrayOfArray[which].length);
-      console.log(password);
-      password = password + arrayOfArray[which][chosen];
+      // console.log(password);
+      if (i === 0) {
+        password = arrayOfArray[which][chosen];
+      }
+      else {
+        password = password + arrayOfArray[which][chosen];
+      }
+      
       // console.log(password);
     }
     return password;
   }
 }
-// Math.floor(Math.random() * passwordArray.length)
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
